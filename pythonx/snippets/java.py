a318d3f700e6d4a1183c8879834ce3f50a9500bf
Egor Kovetskiy <e.kovetskiy@gmail.com>
2019-11-30 14:55:56 +0300

    add more guesses for java

--- pythonx/snippets/java.py
+++ pythonx/snippets/java.py
@@ -1,7 +1,11 @@
 import vim
+import os
+import os.path
+import px
+import px.buffer
 
-def guess_package_from_file_name():
-    path = vim.eval('expand("%:p:h")')
+def guess_package_name_from_file_name():
+    path = os.path.dirname(px.buffer.get().name)
     src = path.find('/src/')
     if src != -1:
         path = path[src+len('/src/'):]
@@ -9,6 +13,23 @@ def guess_package_from_file_name():
         if java != -1:
             path = path[java+len('/java/'):]
     else:
-        path = vim.eval('%:h')
+        path = os.path.basename(path)
 
     return path.replace('/', '.')
+
+def guess_class_name_from_file_name():
+    return os.path.basename(px.buffer.get().name).replace('.java', '')
+
+def guess_imports_from_file_name():
+    class_name = guess_class_name_from_file_name()
+
+    imports = []
+    if class_name.endswith('Response') or class_name.endswith('Request'):
+        imports.append("javax.xml.bind.annotation.*")
+
+    contents = ""
+    if len(imports) > 0:
+        contents = "\n".join(map(lambda x: "import "+x+";", imports))
+        contents = "\n" + contents + "\n"
+        return contents
+    return ""
