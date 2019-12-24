e9dadd44492908efbb613267c002ffa98be93e6b
Egor Kovetskiy <e.kovetskiy@gmail.com>
2019-12-24 23:08:45 +0300

    java: guess type based on current dir

--- pythonx/snippets/java.py
+++ pythonx/snippets/java.py
@@ -3,6 +3,50 @@ import os
 import os.path
 import px
 import px.buffer
+import re
+
+DEFAULT_TYPE_TOKEN = "class"
+
+def guess_type_from_dir():
+    path = os.path.dirname(px.buffer.get().name)
+
+    # key = (class|interface)
+    # value = number of mentions
+    result = {}
+    for dirpath, dnames, fnames in os.walk(path):
+        for name in fnames:
+            fullname = os.path.join(dirpath, name)
+            token = get_type_from_file(fullname)
+            if token in result:
+                result[token] += 1
+            else:
+                result[token] = 0
+
+    max_token = DEFAULT_TYPE_TOKEN
+    max_mentions = 0
+    for item in result.items():
+        token, mentions = item
+        if mentions > max_mentions:
+            max_token = token
+
+    return max_token
+
+
+def get_type_from_file(path):
+    with open(path) as file:
+        for line in file:
+            matches = re.match(
+                r'(public|private)\s*(static)?\s*(class|interface)',
+                line
+            )
+
+            if not matches:
+                continue
+
+            token = matches.group(3)
+            return token
+    return DEFAULT_TYPE_TOKEN
+
 
 def guess_package_name_from_file_name():
     path = os.path.dirname(px.buffer.get().name)
